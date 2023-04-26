d381835915fd9bba21fce037f154b2a0ff0e97bc
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-26 14:13:59 +0800

    [FE] - [feat] add card display in home screen

--- alterbook-mobile/src/screens/HomeScreen.tsx
+++ alterbook-mobile/src/screens/HomeScreen.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import MainLayout from '../layouts/MainLayout';
+import CardPost from '../components/CardPost';
 import tw from '../styles/tailwind';
-import {ScrollView, View, Text} from 'react-native';
+import {ScrollView} from 'react-native';
 
 const HomeScreen = (): JSX.Element => {
   return (
     <MainLayout>
       <ScrollView style={tw`w-full h-full`}>
-        <View style={tw`flex-col w-full h-full`}>
-          <Text style={tw`text-regular`}>Home Screen</Text>
-        </View>
+        <CardPost />
       </ScrollView>
     </MainLayout>
   );
