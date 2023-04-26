eae2d15cb689d4142c5ed7393efa8d4a6ad488d1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-26 12:56:23 +0800

    [FE] - [feat] setup everything and add home screen display (#2)

---
+++ alterbook-mobile/src/components/NavBar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import tw from '../styles/tailwind';
+import {FeatherIcon} from '../utils/Icons';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+import {navbar} from '../config/Paths';
+
+const NavBar = (): JSX.Element => {
+  return (
+    <View
+      style={tw`flex-row items-center justify-between w-full p-5 border-b border-accent-2`}>
+      <View style={tw`flex-1 flex-row items-center justify-start`}>
+        <Text style={tw`text-regular text-xl`}>alterbook</Text>
+      </View>
+      <View style={tw`flex-1 flex-row items-center justify-end`}>
+        {navbar.map(
+          (_nav: {
+            type: string;
+            name: string;
+            icon: string;
+            screen: string;
+          }) => (
+            <React.Fragment key={_nav.name}>
+              {_nav.type === 'link' ? (
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  style={tw`mx-2`}
+                  onPress={() => console.log(_nav.name)}>
+                  <FeatherIcon size={20} name={_nav.icon} color="#E8EAED" />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  style={tw`mx-2`}
+                  onPress={() => console.log('Logout Functionality')}>
+                  <FeatherIcon size={20} name={_nav.icon} color="#E8EAED" />
+                </TouchableOpacity>
+              )}
+            </React.Fragment>
+          ),
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default NavBar;
---
+++ alterbook-mobile/tailwind.config.js
@@ -0,0 +1,52 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#2E3134',
+        'accent-2': '#5F6368',
+        'accent-3': '#E8EAED',
+        'accent-4': '#27C52C',
+      },
+      fontFamily: {
+        'inconsolata-regular': 'Inconsolata-Regular',
+        'inconsolata-light': 'Inconsolata-Light',
+        'inconsolata-medium': 'Inconsolata-Medium',
+        'inconsolata-semibold': 'Inconsolata-SemiBold',
+        'inconsolata-bold': 'Inconsolata-Bold',
+        'inconsolata-black': 'Inconsolata-Black',
+      },
+    },
+  },
+  plugins: [
+    plugin(({addUtilities}) => {
+      addUtilities({
+        'text-regular': {
+          fontFamily: 'Inconsolata-Regular',
+          color: '#E8EAED',
+        },
+        'text-light': {
+          fontFamily: 'Inconsolata-Light',
+          color: '#E8EAED',
+        },
+        'text-medium': {
+          fontFamily: 'Inconsolata-Medium',
+          color: '#E8EAED',
+        },
+        'text-semibold': {
+          fontFamily: 'Inconsolata-SemiBold',
+          color: '#E8EAED',
+        },
+        'text-bold': {
+          fontFamily: 'Inconsolata-Bold',
+          color: '#E8EAED',
+        },
+        'text-black': {
+          fontFamily: 'Inconsolata-Black',
+          color: '#E8EAED',
+        },
+      });
+    }),
+  ],
+};
