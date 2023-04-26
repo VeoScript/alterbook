d462ef69cb2132f3cbce465f70c0bc004026d78e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-26 14:14:26 +0800

    [FE] - [feat] add card display in home screen (#5)

--- alterbook-mobile/src/components/CardPost.tsx
+++ alterbook-mobile/src/components/CardPost.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import tw from '../styles/tailwind';
+import {FeatherIcon} from '../utils/Icons';
 import {View, Text} from 'react-native';
 
 const CardPost = () => {
   return (
-    <View>
-      <Text>Card</Text>
+    <View
+      style={tw`flex-row w-full overflow-hidden border-b border-accent-2 p-5`}>
+      <View style={tw`flex-col items-start mr-3`}>
+        <View style={tw`flex-col w-full my-1`}>
+          <FeatherIcon size={20} name="heart" color="#E8EAED" />
+          <Text style={tw`mt-1 text-regular text-xs text-neutral-400`}>
+            11k
+          </Text>
+        </View>
+        <View style={tw`flex-col w-full my-1`}>
+          <FeatherIcon size={20} name="message-square" color="#E8EAED" />
+          <Text style={tw`mt-1 text-regular text-xs text-neutral-400`}>
+            500
+          </Text>
+        </View>
+      </View>
+      <View style={tw`flex-col items-start ml-3`}>
+        <View style={tw`w-full`}>
+          <Text style={tw`text-light text-sm`}>
+            This is the text caption created by the fuckin user...
+          </Text>
+        </View>
+        <View style={tw`flex-col w-full my-3`}>
+          <Text style={tw`flex-1 my-1 text-light text-sm text-accent-4`}>
+            @username
+          </Text>
+          <Text style={tw`flex-1 my-1 text-light text-xs text-neutral-400`}>
+            April 25, 2023
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
