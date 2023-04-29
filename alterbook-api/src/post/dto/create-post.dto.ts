ba21d3abf3f51b787be9f9390d5fbdbb80b0bcc9
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-29 17:04:48 +0800

    [BE] - [chore] add created at and updated at in each models (#23)

--- alterbook-api/src/post/dto/create-post.dto.ts
+++ alterbook-api/src/post/dto/create-post.dto.ts
@@ -1,4 +1,4 @@
 import { OmitType } from '@nestjs/mapped-types';
 import { PostEntity } from '../entities/post.entity';
 
-export class CreatePostDto extends OmitType(PostEntity, ['id', 'userId']) {}
+export class CreatePostDto extends OmitType(PostEntity, ['id', 'userId', 'created_at', 'updated_at']) {}
